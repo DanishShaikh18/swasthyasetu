923b3545adf584103d7235062673afd886b6c838
Danish <danish89761@gmail.com>
2026-03-29 10:15:53 +0530

    Allow Origing List issue - some duplicate unnecessary values were there

--- backend/app/main.py
+++ backend/app/main.py
@@ -27,14 +27,6 @@ app = FastAPI(
 app.add_middleware(GZipMiddleware, minimum_size=1000)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://localhost:5174",
-        "http://localhost:5175",
-        "http://localhost:5176",
-        "http://localhost:5177",
-        "http://localhost:5178",
-    ],
     allow_origins=["*"],
     allow_credentials=False,
     allow_methods=["*"],
